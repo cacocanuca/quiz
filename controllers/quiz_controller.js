f1e1ee5871c14faf6bc4496feb1787454ebdc7f1
cacocanuca <cacocanuca@hotmail.com>
2015-06-22 22:11:35 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,10 +15,13 @@ exports.load = function(req, res, next, quizId) {
 // GET /quizes
 exports.index = function(req, res) {
   // Si existe query en la ruta, define una variable search con ella, incluyendo '%' al principio, al final y reemplazando los espacios en blanco
-  var search = (req.query.search)? '%' + req.query.search.replace(/ +/g,'%') + '%' : req.query.search;
+  // var search = (req.query.search)? '%' + req.query.search.replace(/ +/g,'%') + '%' : req.query.search;
   // Busca preguntas que contengan la variable search, ordenadas alfabeticamente
-  models.Quiz.findAll({where: {pregunta: {like :search}}, order:['pregunta']}).then(
-  //models.Quiz.findAll().then(
+  // Mi primera versión:
+  // models.Quiz.findAll({where: {pregunta: {like :search}}, order:['pregunta']}).then(
+  // Otro participante en el curso lo hace así:
+  // models.Quiz.findAll({where:["pregunta like ?", search], order: 'pregunta ASC'}).then(
+  models.Quiz.findAll({ order: 'pregunta' }).then(
     function(quizes) {
       res.render('quizes/index.ejs', {quizes: quizes});
   }).catch(function(error) { next(error);});
@@ -36,4 +39,23 @@ exports.answer = function(req, res) {
       resultado ='Correcto';
     }
     res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(  //crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  // guaarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    console.log(quiz);
+    res.redirect('/quizes');
+  })  // Redirección HTTP (URL relativo) lista de preguntas
 };
